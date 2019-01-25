224e6a6fa5a2b973b38e010f73ba008a249249e2
DavidCouronne <couronne.david@gmail.com>
2019-01-25 21:29:02 +0100

    Clean Repositoy- migration Vuetify

--- components/Sidebar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <aside class="sidebar">
-
-        <nav class="nav-links">
-          <nuxt-link class="nav-link" to="/hello">Hello</nuxt-link>
-        </nav>
-      
-    </aside>
-</template>
--- plugins/global.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import Markdown from '@mathssyfy/plugin-markdown'
-
-Vue.component("Markdown", Markdown)
